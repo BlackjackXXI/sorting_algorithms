df7f6a4bdc0ead814215384e954da6f251214f00
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-01-22 22:09:52 +0000

    g3

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,23 @@
 #include "sort.h"
 
 /**
+ * swap_nodes - Swap two nodes in a doubly linked list.
+ * @lhs: A pointer to the node before rhs.
+ * @rhs: A pointer to the node after lhs.
+ */
+void swap_nodes(listint_t *lhs, listint_t *rhs)
+{
+	if (rhs->next)
+		rhs->next->prev = lhs;
+	if (lhs->prev)
+		lhs->prev->next = rhs;
+	lhs->next = rhs->next;
+	rhs->next = lhs;
+	rhs->prev = lhs->prev;
+	lhs->prev = rhs;
+}
+
+/**
  * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
  *                       using the Insertion Sort algorithm.
  * @list: A double pointer to the head of the list.
@@ -28,21 +45,4 @@ void insertion_sort_list(listint_t **list)
 			}
 		}
 	}
-}
-
-/**
- * swap_nodes - Swap two nodes in a doubly linked list.
- * @lhs: A pointer to the node before rhs.
- * @rhs: A pointer to the node after lhs.
- */
-void swap_nodes(listint_t *lhs, listint_t *rhs)
-{
-	if (rhs->next)
-		rhs->next->prev = lhs;
-	if (lhs->prev)
-		lhs->prev->next = rhs;
-	lhs->next = rhs->next;
-	rhs->next = lhs;
-	rhs->prev = lhs->prev;
-	lhs->prev = rhs;
 }
