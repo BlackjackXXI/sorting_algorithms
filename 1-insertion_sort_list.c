b63270d06baf593da31a34ea8bff1d21436c8e99
BlackjackXXI <oussama.bouaoued@gmail.com>
2024-01-22 22:08:42 +0000

    g3

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using the Insertion Sort algorithm.
+ * @list: A double pointer to the head of the list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr;
+	listint_t *next;
+	listint_t *prev;
+
+	if (list != NULL && *list != NULL)
+	{
+		next = (*list)->next;
+		while ((curr = next))
+		{
+			next = curr->next;
+			prev = curr->prev;
+			while (prev && prev->n > curr->n)
+			{
+				swap_nodes(prev, curr);
+				prev = curr->prev;
+				if (!prev)
+					*list = curr;
+				print_list(*list);
+			}
+		}
+	}
+}
+
+/**
+ * swap_nodes - Swap two nodes in a doubly linked list.
+ * @lhs: A pointer to the node before rhs.
+ * @rhs: A pointer to the node after lhs.
+ */
+void swap_nodes(listint_t *lhs, listint_t *rhs)
+{
+	if (rhs->next)
+		rhs->next->prev = lhs;
+	if (lhs->prev)
+		lhs->prev->next = rhs;
+	lhs->next = rhs->next;
+	rhs->next = lhs;
+	rhs->prev = lhs->prev;
+	lhs->prev = rhs;
+}
